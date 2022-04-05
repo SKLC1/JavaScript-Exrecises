25d60937e9a2cc0739fa3eb1963e860e93fd98f2
SKLC1 <segevklein2000@gmail.com>
2022-04-05 14:43:12 +0300

    exrecises 1 2

---
+++ exrecise 1.1 (types)/app.js
@@ -0,0 +1,8 @@
+let myString = 'string';
+let myNumber = 200;
+let object = {make: 'Ford', model: 'Mustang', year: 1969 } ;
+let boolean = true;
+let myNull = null;
+let myUndifined = undefined;
+let symbol = Symbol('foo');
+
